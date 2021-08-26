4a6456687d8030b95eb60d6304ffb24ca8e07db6
Chung-Lun <chunglunlu@gmail.com>
2021-08-26 10:04:53 +0000

    Add note

--- server/src/main/kotlin/com/clluv/chat/server/Connection.kt
+++ server/src/main/kotlin/com/clluv/chat/server/Connection.kt
@@ -9,6 +9,9 @@ import java.util.concurrent.atomic.*
  *                including the incoming and outgoing channels, convenience methods for communication.
  */
 class Connection(val session: DefaultWebSocketSession) {
+    //fun1 id
+    //name fun2(fun1)
+
     companion object {
         // AtomicInteger: thread-safe data structure for the counter.
         // To ensure 2 users will never receive the same ID even when their Connection objects are created simultaneously on separate threads.
